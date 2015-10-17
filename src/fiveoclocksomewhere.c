138834073c1279092f5127af28fd5a8e6fbeee74
Scott Underwood <scunderwood@gmail.com>
2015-10-17 11:42:39 -0400

    Fix if js failed to init
    Wouldn't fetch proper offset from phone, leading to a situation where incorrect location was shown

--- src/fiveoclocksomewhere.c
+++ src/fiveoclocksomewhere.c
@@ -9,6 +9,7 @@ static TextLayer *s_label_layer;
 static TextLayer *s_city_layer;
 
 static int32_t s_timezone_offset = 0;
+static bool s_received = false;
 
 static char *std[] = {"London","Paris","Athens","Moscow","Abu Dhabi",
                          "Calcutta","Kathmandu","Bangkok","Singapore",
@@ -22,14 +23,6 @@ static char *dst[] = {"Liberia","London","Brussels","Saint-Petersburg","Abu Dhab
                          "Honolulu","Alaska","Adamstown","San Francisco","Salt Lake City",
                          "Mexico City","New York","Buenos Aires","Sandwich Islands","Cabo Verde"};
 
-
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  Tuple *t = dict_find(iterator, KEY_TZ_OFFSET);
-  s_timezone_offset = t->value->int32;
-}
-
-
-
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -37,15 +30,13 @@ static void update_time() {
   // Create a long-lived buffer
   static char buffer[] = "00:00";
   // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
+  if(clock_is_24h_style()) {
     //Use 2h hour format
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     //Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
-
-
   int difference = 17 - ((*tick_time).tm_hour - s_timezone_offset);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Offset: %d", difference);
   int month = (*tick_time).tm_mon;
@@ -57,13 +48,23 @@ static void update_time() {
   if(difference<0){
     difference+=24;
   }
-  if(is_dst){
-    text_layer_set_text(s_city_layer, dst[difference]);
-  }else{
-    text_layer_set_text(s_city_layer, std[difference]);
-  }
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  if(s_received){
+    text_layer_set_text(s_label_layer, "Its five o'clock in");
+    if(is_dst){
+      text_layer_set_text(s_city_layer, dst[difference]);
+    }else{
+      text_layer_set_text(s_city_layer, std[difference]);
+    }
+  }
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  Tuple *t = dict_find(iterator, KEY_TZ_OFFSET);
+  s_timezone_offset = t->value->int32;
+  s_received = true;
+  update_time();
 }
 
 static void main_window_load(Window *window) {
@@ -71,24 +72,20 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
   s_label_layer = text_layer_create(GRect(0, 115, 144, 20));
   text_layer_set_background_color(s_label_layer, GColorClear);
   text_layer_set_text_color(s_label_layer, GColorBlack);
-  text_layer_set_text(s_label_layer, "Its five o'clock in");
-
+  
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(s_label_layer, GTextAlignmentCenter);
-  
+
   s_city_layer = text_layer_create(GRect(0, 135, 144, 30));
   text_layer_set_background_color(s_city_layer, GColorClear);
   text_layer_set_text_color(s_city_layer, GColorBlack);
-
   text_layer_set_font(s_city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter); 
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_label_layer));
